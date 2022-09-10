c4d9002cad0e2ab86710e5344e54cd2c3b731a9e
Pratiksh Y Patel <31757911+prat1991@users.noreply.github.com>
2022-09-10 11:51:46 -0500

    V2 for SpringBootJenkinsRestApiApp

--- src/main/java/prat/springbootjenkinsrestapi/SpringBootJenkinsRestApiApplication.java
+++ src/main/java/prat/springbootjenkinsrestapi/SpringBootJenkinsRestApiApplication.java
@@ -19,7 +19,7 @@ public class SpringBootJenkinsRestApiApplication {
 	}
 
 	public static void main(String[] args) {
-		logger.info("SpringBootJenkinsRestApiApplication is running ...");
+		logger.info("SpringBootJenkinsRestApiApplication is runningssssssssssssssss ...");
 		SpringApplication.run(SpringBootJenkinsRestApiApplication.class, args);
 
 	}
